0eab04dc4300ccd9d75bc04b07a50cbbaced16bc
manojkrjoshi <44704973+manojkrjoshi@users.noreply.github.com>
2020-01-08 18:14:44 +0530

    ip updated in graphql file

--- src/graphQLClient.tsx
+++ src/graphQLClient.tsx
@@ -2,7 +2,7 @@ import { ApolloClient, createNetworkInterface } from 'react-apollo';
 
 const networkInterface = createNetworkInterface({
   // uri: 'http://100.81.3.25:8080/graphql'
-  uri: 'http://localhost:9091/graphql'
+  uri: 'http://100.81.3.26:9091/graphql'
 });
 export const gQLClient = new ApolloClient({
   networkInterface: networkInterface
